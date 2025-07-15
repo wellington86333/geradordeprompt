99e2a7324d7435160b0d6468f4e774514f49221d
Wellington Brito <hectorhp70@gmail.com>
2025-07-15 19:08:17 +0000

    Retirar link que direciona para minha pagina

--- src/components/app/MyYoutubeChannelSection.tsx
+++ src/components/app/MyYoutubeChannelSection.tsx
@@ -6,7 +6,7 @@ import { Youtube } from 'lucide-react';
 const MyYoutubeChannelSection = () => {
   return (
     <section className="my-16">
-      <a href="https://www.youtube.com/@Aleat%C3%B3riamenteredund%C3%A2ntico" target="_blank" rel="noopener noreferrer" className="block w-full max-w-4xl mx-auto group">
+      <div className="block w-full max-w-4xl mx-auto group">
         <Card className="overflow-hidden bg-gradient-to-tr from-red-900 via-black to-black border-red-800/50 shadow-2xl shadow-red-500/10 group-hover:shadow-red-500/20 transition-shadow duration-300">
           <CardHeader className="text-center p-8">
             <div className="mx-auto">
@@ -16,7 +16,7 @@ const MyYoutubeChannelSection = () => {
           <CardContent className="p-8 pt-0">
           </CardContent>
         </Card>
-      </a>
+      </div>
     </section>
   );
 };
